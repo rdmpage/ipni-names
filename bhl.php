f80a24c24384fd1c5b9ca0bd4f834a617e898d8e
Roderic Page <rdmpage@gmail.com>
2014-06-23 09:19:58 +0100

    Handle case where there is no XML more gracefully

--- bhl.php
+++ bhl.php
@@ -7,26 +7,35 @@ require_once(dirname(__FILE__) . '/lib.php');
 $PageID = $_GET['PageID'];
 $term = $_GET['term'];
 
+$imageUrl = 'http://biostor.org/bhl_image.php?PageID=' . $PageID;
+
 $xml = get('http://biostor.org/bhl_page_xml.php?PageID=' . $PageID);
 
-$xp = new XsltProcessor();
-$xsl = new DomDocument;
-$xsl->load(dirname(__FILE__) . '/djvu2html.xsl');
-$xp->importStylesheet($xsl);	
-
-// Load XML
-$dom= new DOMDocument;
-$dom->loadXML($xml);
-$xpath = new DOMXPath($dom);
-
-// Export HTML with background image using XSLT
-$imageUrl = 'http://biostor.org/bhl_image.php?PageID=' . $PageID;
-$xp->setParameter('', 'imageUrl', $imageUrl);
-$xp->setParameter('', 'widthpx', 500);
-$xp->setParameter('', 'term', $term);
-
-$html = $xp->transformToXML($dom);
-
+if ($xml != '<?xml version="1.0" ?>')
+{
+	
+	$xp = new XsltProcessor();
+	$xsl = new DomDocument;
+	$xsl->load(dirname(__FILE__) . '/djvu2html.xsl');
+	$xp->importStylesheet($xsl);	
+	
+	// Load XML
+	$dom= new DOMDocument;
+	$dom->loadXML($xml);
+	$xpath = new DOMXPath($dom);
+	
+	// Export HTML with background image using XSLT
+	
+	$xp->setParameter('', 'imageUrl', $imageUrl);
+	$xp->setParameter('', 'widthpx', 500);
+	$xp->setParameter('', 'term', $term);
+	
+	$html = $xp->transformToXML($dom);
+}
+else
+{
+	$html = '<span style="background-color:orange;color-white;">Warning: no XML!</span><img style="border:1px solid rgb(228,228,228);" src="' . $imageUrl . '" width="500" />';
+}
 $data = new stdclass;
 $data->html = $html;
 
