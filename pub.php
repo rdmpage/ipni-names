7368d6247bf082afdddefa7b01a02da1e1b62e76
Roderic Page <rdmpage@gmail.com>
2018-11-21 13:29:36 +0000

    PHP 7 changes

--- pub.php
+++ pub.php
@@ -8,7 +8,7 @@ require_once(dirname(__FILE__) . '/CiteProc.php');
 
 
 //--------------------------------------------------------------------------------------------------
-$db = NewADOConnection('mysql');
+$db = NewADOConnection('mysqli');
 $db->Connect("localhost", 
 	$config['db_user'] , $config['db_passwd'] , $config['db_name']);
 
