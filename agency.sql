bf6882ad6d0850741f5ae51ad1eba1525b985e1e
Roderic Page <rdmpage@gmail.com>
2020-10-06 11:53:07 +0100

    Get registration agency for DOIs

---
+++ agency.sql
@@ -0,0 +1,408 @@
+-- 10.30972/bon.121-41407
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5962/bhl.part.24281
+-- 10.5169/seals-73287
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-73288
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-73289
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-73585
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.30972/bon.121-41405
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.101-41452
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.101-41453
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.101-41455
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 2.36 seconds
+-- 10.30972/bon.111-43944
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2161546
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5962/bhl.title.574
+-- 10.5962 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.5962%";
+-- 10.30972/bon.71-41428
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.1080/00378941.1907.10833386
+-- 10.1080 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.1080%";
+-- 10.5962/bhl.part.5062
+-- 10.5962/bhl.part.24283
+-- 10.30972/bon.81-43559
+-- 10.30972/bon.641519
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5962/bhl.part.9605
+-- sleeping for 1.12 seconds
+-- 10.5962/bhl.part.9613
+-- 10.5962/bhl.part.4322
+-- 10.30972/bon.121-41403
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.91-21472
+-- 10.5962/bhl.part.17235
+-- 10.30972/bon.71-41430
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.71-41429
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5169/seals-64301
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 
+-- 10.30972/bon.35-71444
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 1.26 seconds
+-- 10.30972/bon.141449
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.391447
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.51-71524
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.310-131437
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5169/seals-64305
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-67187
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-69128
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-69707
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-70315
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-70314
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- sleeping for 1.86 seconds
+-- 10.5169/seals-70937
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-70944
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-73028
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.30972/bon.3141520
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.32-41441
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.32-41442
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5169/seals-65880
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-74015
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5962/bhl.part.24282
+-- 10.30972/bon.141451
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 1.66 seconds
+-- 10.30972/bon.35-71445
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.62-31510
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.611506
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.62-31513
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5962/bhl.part.4321
+-- 10.30972/bon.71-41425
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.91-21469
+-- 10.30972/bon.91-21470
+-- 10.30972/bon.91-21466
+-- 10.30972/bon.91-21468
+-- sleeping for 1.54 seconds
+-- 10.30972/bon.71-41426
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.71-41427
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.93-41482
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.93-41484
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.93-41488
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.93-41489
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.93-41491
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.121-41404
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.121-41408
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.121-41409
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 2.56 seconds
+-- 10.30972/bon.121-41413
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.411569
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5962/bhl.part.17229
+-- 10.30972/bon.131-41396
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.131-41397
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.131-41399
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.131-41400
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.141-21387
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.141-21390
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.143-41382
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 1.77 seconds
+-- 10.2179/05-5.1
+-- 10.2179 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.2179%";
+-- 10.30972/bon.1711356
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1711359
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1811343
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1811346
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1811348
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.2179/12-043
+-- 10.2179 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.2179%";
+-- 10.5169/seals-74715
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.30972/bon.151-289
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.151-290
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 2.36 seconds
+-- 10.30972/bon.153-497
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.2179/0008-7475(2007)72[58:taloab]2.0.co;2
+-- 10.2179 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.2179%";
+-- 10.30972/bon.163-4150
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.163-4157
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1721354
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1911331
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1911332
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1911333
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.1911336
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.2179/10-049.1
+-- 10.2179 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.2179%";
+-- sleeping for 1.7 seconds
+-- 10.30972/bon.32-41440
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5169/seals-65252
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-66499
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-65876
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-65244
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-67856
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-68570
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-68581
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-69126
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-69709
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- sleeping for 2.63 seconds
+-- 10.5169/seals-69710
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-67862
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-67202
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-67851
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-70301
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-70317
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-71752
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.30972/bon.91-21474
+-- 10.5169/seals-72637
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.5169/seals-72639
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- sleeping for 1.99 seconds
+-- 10.5169/seals-72644
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.30972/bon.2311239
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2221250
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.2179/10-009.1
+-- 10.2179 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.2179%";
+-- 10.30972/bon.2211257
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2111317
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2221242
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2111315
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2111318
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.2121308
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 2.18 seconds
+-- 10.30972/bon.2121307
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5169/seals-88908
+-- 10.5169 DataCite
+UPDATE names SET doi_agency="datacite" WHERE doi LIKE "10.5169%";
+-- 10.30972/bon.24185
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.242257
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.241208
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.242238
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.30972/bon.241209
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- 10.5962/bhl.part.7612
+-- 10.5962 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.5962%";
+-- 10.5565/rev
+-- 10.30972/bon.2622569
+-- 10.30972 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.30972%";
+-- sleeping for 1.64 seconds
+-- 10.14522/darwiniana.2017.51.733
+-- 10.14522 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.14522%";
+-- 10.20324/nelumbo
+-- 10.3417/D1700005
+-- 10.3417 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.3417%";
+-- 10.14522/darwiniana.2017.51.716
+-- 10.14522 Crossref
+UPDATE names SET doi_agency="crossref" WHERE doi LIKE "10.14522%";
+-- 10.22092/ijb.2017.114798.1175
+-- 10.7677/ynzwyj201515043
